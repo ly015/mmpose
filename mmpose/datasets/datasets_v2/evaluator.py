8f86afe9918088bf196638ed7b5888ca1cc15382
ly015 <liyining0712@gmail.com>
2021-09-27 16:57:41 +0800

    add evaluator base

--- mmpose/datasets/datasets_v2/evaluator.py
+++ mmpose/datasets/datasets_v2/evaluator.py
@@ -1,9 +1,29 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import functools
 from abc import ABCMeta, abstractclassmethod
+
+import numpy as np
 
 
 def with_cache(func):
-    pass
+
+    @functools.wraps(func)
+    def wrapped_func(evaluator, *args, **kwargs):
+
+        if evaluator.use_cache:
+
+            key = func.__name__
+            if key in evaluator.cache:
+                return evaluator.cache[key].clone()
+            else:
+                value = func(evaluator, *args, **kwargs)
+                evaluator.cache[key] = value.clone()
+                return value
+
+        else:
+            return func(evaluator, *args, **kwargs)
+
+    return wrapped_func
 
 
 class BaseEvaluator(metaclass=ABCMeta):
@@ -26,4 +46,33 @@ class BaseEvaluator(metaclass=ABCMeta):
 
     def evaluate(self) -> dict:
 
-        return {}
+        results = {}
+
+        assert self.results
+        for k in self.results[0].keys():
+            results[k] = np.mean([result[k] for result in self.results])
+
+        self.results = []
+        return results
+
+
+class ComposedEvaluator(BaseEvaluator):
+
+    def __init__(self, *args, **kwargs):
+
+        # build evaluators
+        self._evaluators = []
+        ...
+
+        # share cache
+        for evaluator in self._evaluators:
+            evaluator.cache = self.cache
+
+    def _process(self, data: dict, output: dict) -> dict:
+        result = {}
+
+        for evaluator in self._evaluators:
+            _result = evaluator._process(data, output)
+            result.update(_result)
+
+        return result
