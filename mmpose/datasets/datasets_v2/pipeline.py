20b8ab81ee2f49ed59254e52ede22796026ec645
ly015 <liyining0712@gmail.com>
2021-10-25 15:50:05 +0800

    [wip] update transform

--- mmpose/datasets/datasets_v2/pipeline.py
+++ mmpose/datasets/datasets_v2/pipeline.py
@@ -8,31 +8,64 @@ from typing import Any, Optional
 class Transform(metaclass=ABCMeta):
 
     def __init__(self,
-                 key_mapping: Optional[dict] = None,
-                 strict_key_mapping=False):
+                 input_mapping: Optional[dict] = None,
+                 output_mapping: Optional[dict] = None,
+                 inplace=False,
+                 strict=False):
+        """
+        Args:
+            input_mapping (dict): A dict that defines the input key mapping.
+                The keys corresponds to the inner key (i.e. kwargs of the
+                `transform` method), and the values corresponds to the outer
+                keys (i.e. the keys of the data/results).
+            output_mapping(dict): A dict that defines the output key mapping.
+                The keys corresponds to the inner key (i.e. the keys of the
+                output dict of the `transform` method), and the values
+                corresponds to the outer keys (i.e. the keys of the
+                data/results).
+        """
 
-        self.strict_key_mapping = strict_key_mapping
+        self._inplace = inplace
+        self._strict = strict
 
-        if key_mapping is None:
-            self._key_mapping = copy.deepcopy(self._default_key_mapping)
+        self._input_mapping = copy.deepcopy(self.get_default_input_mapping())
+        if input_mapping is not None:
+            self._input_mapping.update(input_mapping)
+            self._static_check_input_mapping(self._input_mapping)
+
+        if output_mapping is None:
+            if self.inplace:
+                self._output_mapping = self._input_mapping
+            else:
+                self._output_mapping = None
         else:
-            self._key_mapping = key_mapping
-
-        self._static_check_key_mapping(self._key_mapping)
+            self._output_mapping = output_mapping
 
     @property
-    def default_key_mapping(self):
-        if not hasattr(self, '_default_key_mapping'):
+    def strict(self):
+        return self._strict
 
-            self._default_key_mapping = {
-                'input': {
-                    key: key
-                    for key in inspect.signature(self.transform).parameters
-                },
-                'output': None
+    @property
+    def inplace(self):
+        return self._inplace
+
+    def get_default_input_mapping(self) -> dict:
+        if not hasattr(self, '_default_input_mapping'):
+
+            self._default_input_mapping = {
+                key: key
+                for key in inspect.signature(self.transform).parameters
             }
 
-        return self._default_key_mapping
+        return self._default_input_mapping
+
+    @property
+    def get_default_output_mapping(self):
+        if self.inplace:
+            # inplace mode: output mapping is the same as input mapping
+            return self.get_default_input_mapping()
+        # no output mapping
+        return None
 
     def _static_check_input_mapping(self, input_mapping):
 
@@ -48,25 +81,7 @@ class Transform(metaclass=ABCMeta):
                              f'Got unexpected keys {unexpected_keys}, '
                              f'expect {default_input_mapping.keys()}.')
 
-    def _dynamic_check_input_mapping(self, input_mapping, result):
-        missing_keys = input_mapping.values() - result.keys()
-        if missing_keys:
-            raise RuntimeError('Dynamic input_mapping check failed: '
-                               f'keys {missing_keys} missing in data.')
-
-    def _dynamic_check_output_key_mapping(self, output_mapping, output):
-
-        if output_mapping is None:
-            return
-
-        missing_keys = output_mapping.values() - output.keys()
-        if missing_keys:
-            raise RuntimeError('Dynamic output_mapping check failed.'
-                               f'keys {missing_keys} missing in the '
-                               'transform output')
-
-    def collect_input(self, data: dict,
-                      input_mapping: dict[str, Any]) -> dict[str, Any]:
+    def collect_input(self, data: dict, input_mapping: dict) -> dict[str, Any]:
         """Collect input of `transform` function from the data according to the
         input mapping."""
 
@@ -98,30 +113,94 @@ class Transform(metaclass=ABCMeta):
         return _collect(data, input_mapping)
 
     def collect_output(self, output: dict,
-                       output_mapping: dict[str, Any]) -> dict[str, Any]:
+                       output_mapping: dict) -> dict[str, Any]:
         """Collect items from the `transform` output to update to the data
         flow."""
         ...
 
     def __call__(self, results: dict):
 
-        key_mapping = self._key_mapping
-
-        self._dynamic_check_input_mapping(key_mapping, results)
-
-        kwargs = self.collect_input(results, key_mapping['input'])
+        kwargs = self.collect_input(results, self._input_mapping)
         output = self.transform(**kwargs)
 
-        if key_mapping.get('output', None):
-            self._dynamic_check_output_key_mapping(key_mapping, output)
-            # output = {
-            #     outer_key: output[inner_key]
-            #     for inner_key, outer_key in key_mapping['output'].items()
-            # }
-            output = self.collect_output(output, key_mapping['output'])
+        if self._output_mapping:
+            output = self.collect_output(output, self._output_mapping)
+
         results.update(output)
         return results
 
     @abstractmethod
     def transform(self, **kwargs) -> dict[str, Any]:
         pass
+
+
+class RepeatableTransform(Transform):
+
+    def __init__(self,
+                 input_mapping: Optional[dict] = None,
+                 output_mapping: Optional[dict] = None,
+                 inplace=False,
+                 strict=False,
+                 broadcast=True):
+        super().__init__(input_mapping, output_mapping, inplace, strict)
+        self.broadcast = broadcast
+        self._num_repeat = self._get_repeat_num()
+
+    @property
+    def broadcast(self):
+        return self._broadcast
+
+    def _get_repeat_num(self) -> int:
+        num_repeat = -1
+        for k, v in self._input_mapping.items():
+            if isinstance(v, (list, tuple)):
+                if num_repeat == -1:
+                    num_repeat = len(v)
+                else:
+                    raise ValueError(
+                        'Invalid input_mapping for RepeatableTransform: '
+                        'The length of repeated keys must be consistent, '
+                        f'but got diverse lengths {num_repeat} vs {len(v)}')
+            else:
+                if not self.broadcast:
+                    raise ValueError(
+                        'Invalid input_mapping for RepeatableTransform: '
+                        'only takes sequential outer keys for '
+                        'input key mapping when broadcast==False, '
+                        f'got single outer key "{k}"')
+
+        if num_repeat > 0:
+            if not self._output_mapping:
+                raise ValueError(
+                    'Invalid output_mapping for RepeatableTransform: '
+                    'output_mapping cannot be "None" when input_mapping '
+                    'is repeatable.')
+            for k, v in self._output_mapping.items():
+                if len(v) != num_repeat:
+                    raise ValueError(
+                        'Invalid output_mapping for RepeatableTransform: '
+                        'The length of repeated keys must be consistent with '
+                        f' input_mapping but got diverse lengths {num_repeat} '
+                        f' vs {len(v)}')
+
+        return num_repeat
+
+    def __call__(self, results: dict):
+
+        if self._num_repeat == -1:
+            return super().__call__(results)
+        else:
+            sub_input_mappings = self._slice_key_mapping(self._input_mapping)
+            sub_output_mappings = self._slice_key_mapping(self._output_mapping)
+
+            output = {}
+            for _input_mapping, _output_mapping in zip(sub_input_mappings,
+                                                       sub_output_mappings):
+                kwargs = self.collect_input(results, _input_mapping)
+                _output = self.transform(**kwargs)
+
+                _output = self.collect_output(_output, _output_mapping)
+
+            output.update(_output)
+
+        results.update(output)
